b30e721f1f63f37e69a829adc44bffc5c8cd3568
Patel Tanaka <pateltanaka22@gmail.com>
2017-07-31 17:10:30 +0200

    make it better

--- app/config/index.js
+++ app/config/index.js
@@ -1,15 +1,18 @@
 'use strict'
 
 var init = function () {
-console.log("configurations have been made");
+console.log("configurations have been made");  
 	if(process.env.NODE_ENV === 'production') {
 		
 		return {
 			db: {
 			
-				host: process.env.dbHost,
-				port: process.env.dbPort,
-				name: process.env.dbName
+			username: "Tello",
+        password: "13dochesterS",
+        host: "ds021010.mlab.com",
+        port: "21010",
+        name: "loginapp"
+
 			},
 			sessionSecret: process.env.sessionSecret,
 			
